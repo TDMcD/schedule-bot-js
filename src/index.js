c8a61689cecffd5d21e7ae29ea66056f88d9d456
TDM-Programming <tdm.programming@gmail.com>
2025-03-28 17:40:57 -0400

    Added and Commented Code for Deleting Global Commands

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const {
   MessageFlags,
   ActivityType,
   Events,
+  REST,
+  Routes,
 } = require("discord.js");
 const { CommandKit } = require("commandkit");
 const eventHandler = require("./handlers/eventHandler");
+const getApplicationCommands = require("./utils/getApplicationCommands");
+const { clientId } = require("../config.json");
 require("dotenv").config();
 
 const client = new Client({
@@ -18,6 +22,20 @@ const client = new Client({
     "GuildMessagePolls",
   ],
 });
+
+// For deleting global commands
+// client.on(Events.ClientReady, async () => {
+//   const commands = await client.application.commands.fetch();
+
+//   console.log(commands);
+
+//   const rest = new REST().setToken(process.env.TOKEN);
+
+//   rest
+//     .delete(Routes.applicationCommand(clientId, "1179744763071701002"))
+//     .then(() => console.log("Successfully deleted application command."))
+//     .catch(console.error);
+// });
 
 eventHandler(client);
 
