2ec8d5a44e822335519f32f7a9fbb9ef018be693
TDM-Programming <tdm.programming@gmail.com>
2025-03-28 16:46:08 -0400

    Restructuring

---
+++ src/handlers/eventHandler.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const getAllFiles = require("../utils/getAllFiles");
+const { Client } = require("discord.js");
+
+module.exports = (client) => {
+  const eventFolders = getAllFiles(path.join(__dirname, "..", "events"), true);
+
+  for (const eventFolder of eventFolders) {
+    const eventFiles = getAllFiles(eventFolder);
+    eventFiles.sort((a, b) => a > b);
+    // console.log(eventFiles);
+
+    const eventName = eventFolder.replace(/\\/g, "/").split("/").pop();
+
+    client.on(eventName, async (arg) => {
+      for (const eventFile of eventFiles) {
+        const eventFunction = require(eventFile);
+        await eventFunction(client, arg);
+      }
+    });
+  }
+};
